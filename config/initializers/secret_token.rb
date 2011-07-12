adfc8b7424d3ffaa87b763e4707ae39668e8ee13
Neil McCarthy <nmccarthy@yammer-inc.com>
2011-07-11 22:57:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blankey::Application.config.secret_token = '3ad5bb314e14e3b690a2055e00d0eac682da78b7f29c8c82cfb5947729140fbe17819b5be58a1c073eae9c304a37e6a2bf91ffe7dacb3f3c9cf8e7c0675cfeb6'
